b99f9be8d83aede782860b6b5d02a623a25836b7
mgamamunbd <mgamamunbd@gmail.com>
2022-03-04 13:07:03 +0400

    changed

--- weather/weather.js
+++ weather/weather.js
@@ -1,18 +1,32 @@
 /** @format */
+//error handling
+document.getElementById("errorMessage").style.display = "none";
+
 const API_KEY = `7bd1a650d783aca91002f39377e12b0a`;
 const getWeather = () => {
   const city = document.getElementById("city-name").value;
+  if (!city) {
+    displayError();
+    return;
+  }
+
+  document.getElementById("errorMessage").style.display = "none";
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
   //   console.log(url);
   fetch(url)
     .then((response) => response.json())
     .then((data) => displayTemperature(data));
 };
+
+const displayError = (error) => {
+  document.getElementById("errorMessage").style.display = "block";
+};
 const setInnerText = (id, text) => {
   document.getElementById(id).innerText = text;
+  //   city.innerHTML = "";
 };
 const displayTemperature = (temperature) => {
-  console.log(temperature);
+  //   console.log(temperature);
   setInnerText("city", temperature.name);
   setInnerText("temperature", temperature.main.temp);
   setInnerText("condition", temperature.weather[0].main);
